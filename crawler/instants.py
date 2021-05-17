7961ebaa0a5f2f9a31c4c5a32a6249bd54bd646a
Giovani Moutinho <giovanim@ymail.com>
2021-05-16 21:27:59 -0300

    Add crawler tests

---
+++ crawler/instants.py
@@ -0,0 +1,108 @@
+import re
+
+import requests
+from loguru import logger
+from bs4 import BeautifulSoup
+
+
+class InstantsCrawler:
+    BASE_URL = 'https://www.myinstants.com'
+
+    def get_search_results(self, search):
+        logger.debug(f'Getting search results for "{search}"')
+        html = requests.get(f'{self.BASE_URL}/search?name={search}')
+        soup = BeautifulSoup(html.content, 'html.parser')
+        instants = soup.select('.instant')
+        return instants[:25]
+
+    def get_single_search_result(self, search):
+        results = self.get_search_results(search)
+        return results[0] if results else None
+
+    def get_instant_name(self, instant):
+        instant_link = instant.select_one('.instant-link')
+        instant_name = instant_link.text
+        logger.debug(f'Found instant name: "{instant_name}"')
+        return instant_name
+
+    def get_instant_mp3_link(self, instant):
+        mp3_div = instant.select_one('.small-button')
+        mouse_event = mp3_div.attrs.get('onmousedown')
+        mp3_link = re.search('/media.+.mp3', mouse_event).group(0)
+        full_mp3_link = f'{self.BASE_URL}{mp3_link}'
+        logger.debug(f'Found mp3 link: "{full_mp3_link}"')
+        return full_mp3_link
+
+    def get_instant_link(self, instant):
+        link_attrs = instant.select_one('.instant-link').attrs
+        instant_link = link_attrs['href']
+        full_instant_link = f'{self.BASE_URL}{instant_link}'
+        logger.debug(f'Found instant link: "{full_instant_link}"')
+        return full_instant_link
+
+    def get_instant_details(self, instant):
+        instant_link = self.get_instant_link(instant)
+        html = requests.get(instant_link)
+        soup = BeautifulSoup(html.content, 'html.parser')
+        title = self.get_instant_title(soup)
+        description = self.get_instant_description(soup)
+        likes = self.get_instant_likes(soup)
+        uploader_name = self.get_instant_uploader_name(soup)
+        uploader_url = self.get_instant_uploader_url(soup)
+        views = self.get_instant_views(soup)
+        instant_details = {
+            'title': title,
+            'description': description,
+            'likes': likes,
+            'uploader_name': uploader_name,
+            'uploader_url': uploader_url,
+            'views': views,
+        }
+        logger.debug(f'Found instant details: "{instant_details}"')
+        return instant_details
+
+    def get_instant_title(self, soup):
+        try:
+            return soup.select_one('#instant-page-title').text
+        except AttributeError:
+            return None
+
+    def get_instant_description(self, soup):
+        try:
+            return soup.select_one('#instant-page-description').p.text
+        except AttributeError:
+            return None
+
+    def get_instant_likes(self, soup):
+        try:
+            return soup.select_one('#instant-page-likes').b.text
+        except AttributeError:
+            return None
+
+    def get_instant_uploader_name(self, soup):
+        try:
+            views_div = (
+                soup.select_one('#instant-page-likes').nextSibling.nextSibling
+            )
+            return views_div.a.text
+        except AttributeError:
+            return 'Anonymous'
+
+    def get_instant_uploader_url(self, soup):
+        try:
+            views_div = (
+                soup.select_one('#instant-page-likes').nextSibling.nextSibling
+            )
+            href_attr = views_div.a.attrs.get('href')
+            return f'{self.BASE_URL}{href_attr}'
+        except AttributeError:
+            return None
+
+    def get_instant_views(self, soup):
+        try:
+            views_div = (
+                soup.select_one('#instant-page-likes').nextSibling.nextSibling
+            )
+            return re.search(r'[\d,]+ *views', views_div.text).group(0)
+        except AttributeError:
+            return None
---
+++ tests/test_crawler.py
@@ -0,0 +1,131 @@
+import os
+from unittest import mock
+
+import pytest
+from bs4 import BeautifulSoup
+
+from crawler.instants import InstantsCrawler
+
+
+def get_fixture(file_name: str) -> dict:
+    path = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(path, 'fixtures', file_name)
+    with open(full_path, 'rb') as f:
+        return f.read()
+
+
+@pytest.fixture
+def search_results_page():
+    return get_fixture('search_results.html')
+
+
+@pytest.fixture
+def instant_details_page():
+    return get_fixture('instant_details.html')
+
+
+@pytest.fixture
+def instants_crawler():
+    return InstantsCrawler()
+
+
+@pytest.fixture
+def instant_result(search_results_page, instants_crawler):
+    with mock.patch('crawler.instants.requests.get') as mock_requests:
+        mock_requests.return_value.content = search_results_page
+        instant_result = instants_crawler.get_single_search_result('discord')
+    return instant_result
+
+
+@pytest.fixture
+def soup_instant_details(instant_details_page):
+    return BeautifulSoup(instant_details_page, 'html.parser')
+
+
+def test_instants_crawler_has_base_url():
+    crawler = InstantsCrawler()
+    assert crawler.BASE_URL == 'https://www.myinstants.com'
+
+
+@mock.patch('crawler.instants.requests.get')
+def test_instants_crawler_get_search_results(
+        mock_requests, search_results_page, instants_crawler
+):
+    mock_requests.return_value.content = search_results_page
+
+    results = instants_crawler.get_search_results('discord')
+    assert len(results) == 25
+    assert 'Discord Notification' in results[0].text
+    assert 'discordjoin' in results[1].text
+    assert 'discord call' in results[2].text
+
+
+@mock.patch('crawler.instants.requests.get')
+def test_instants_crawler_get_single_search_result(
+        mock_requests, search_results_page, instants_crawler
+):
+    mock_requests.return_value.content = search_results_page
+
+    result = instants_crawler.get_single_search_result('discord')
+    assert 'Discord Notification' in result.text
+
+
+def test_get_instant_name(instants_crawler, instant_result):
+    instant_name = instants_crawler.get_instant_name(instant_result)
+    assert instant_name == 'Discord Notification'
+
+
+def test_get_instant_mp3_link(instants_crawler, instant_result):
+    instant_mp3_link = instants_crawler.get_instant_mp3_link(instant_result)
+    assert instant_mp3_link == (
+        'https://www.myinstants.com/media/sounds/discord-notification.mp3'
+    )
+
+
+def test_get_instant_link(instants_crawler, instant_result):
+    instant_link = instants_crawler.get_instant_link(instant_result)
+    assert instant_link == (
+        'https://www.myinstants.com/instant/discord-notification-38119/'
+    )
+
+
+def test_get_instant_details(instants_crawler, instant_result):
+    instant_details = instants_crawler.get_instant_details(instant_result)
+    assert instant_details == {
+        'description': None,
+        'likes': '20,526 users',
+        'title': 'Discord Notification',
+        'uploader_name': 'Anonymous',
+        'uploader_url': None,
+        'views': '372,608 views',
+    }
+
+
+def test_get_instant_title(soup_instant_details, instants_crawler):
+    result = instants_crawler.get_instant_title(soup_instant_details)
+    assert result == 'Discord Notification'
+
+
+def test_get_instant_description(soup_instant_details, instants_crawler):
+    result = instants_crawler.get_instant_description(soup_instant_details)
+    assert result is None
+
+
+def test_get_instant_likes(soup_instant_details, instants_crawler):
+    result = instants_crawler.get_instant_likes(soup_instant_details)
+    assert result == '20,526 users'
+
+
+def test_get_instant_views(soup_instant_details, instants_crawler):
+    result = instants_crawler.get_instant_views(soup_instant_details)
+    assert result == '372,608 views'
+
+
+def test_get_instant_uploader_name(soup_instant_details, instants_crawler):
+    result = instants_crawler.get_instant_uploader_name(soup_instant_details)
+    assert result == 'Anonymous'
+
+
+def test_get_instant_uploader_url(soup_instant_details, instants_crawler):
+    result = instants_crawler.get_instant_uploader_url(soup_instant_details)
+    assert result is None
